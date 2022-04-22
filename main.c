5142bb052210de9d75db1c6c2f43e7733f7612a3
gcbragion@gmail.com <gcbragion@gmail.com>
2022-04-22 16:58:20 -0300

    Arquivos finais

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    int contador, quantidade_alunos;
+    float soma, media;
+
+    printf("Programa para calcular as médias dos alunos!\nUsando Vetor de Alocação Estática\n\n");
+    printf("Digite a quantidade de alunos: ");
+    scanf("%d", &quantidade_alunos);
+
+    float notas[quantidade_alunos];
+
+    for (contador = 0; contador < quantidade_alunos; contador++) {
+        printf("Digite a nota do(a) aluno(a) %d: ", contador + 1);
+        scanf("%f", &notas[contador]);
+
+        soma += notas[contador];
+    }
+
+	media = soma / quantidade_alunos;
+
+    printf("\nA média das notas da turma é: %.2f\n", media);
+
+    return 0;
+}
